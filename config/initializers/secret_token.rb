74834f29b9d563f6b74a5fd72b05f3eea86bd481
voldemar <voldemar.duketskiy@gmail.com>
2013-03-13 16:25:03 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KoenigAssault::Application.config.secret_token = 'c5d227b01dd2367a7fc9dec5bc809b7b3ab2a0c841fd171e5656ba11450f1070f6ee0e479633abca7128c34a82cf3adc598c9108635614daa01f8cacef6e3037'
