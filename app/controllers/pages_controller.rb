e1417489d09daa7f15beb9950de52cf0d1180cfc
voldemar <voldemar.duketskiy@gmail.com>
2013-03-14 13:08:17 +0300

    готова главная страница

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,10 @@ class PagesController < ApplicationController
   def index
   	@title = "Штурм Кенигсберга 9 апреля 1945 года"
   	@index_message = t(:index_page_text)
-  	@first_post = ArchivePost.limit(1)
+  	@first_post = ArchivePost.limit(1).reverse_order
   	@top_photos = PhotoArchive.limit(6)
+
+  	@another_posts = ArchivePost.limit(4).offset(1).reverse_order
   end
 
   def photos
